b301dff501a12f65bef5b2b4e653dfe3c30d3d8b
Sanket <sanket.korgaonkar@gmail.com>
2022-04-13 16:49:58 -0400

    Feature: Removed unnecessary code.

--- src/main/java/hogwarts/engineering/cloud/academy/domain/TemperatureDataRepository.java
+++
@@ -1,9 +0,0 @@
-package hogwarts.engineering.cloud.academy.domain;
-
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface TemperatureDataRepository extends CrudRepository<TemperatureData, Long> {
-    public List<TemperatureData> findByTimestamp(Long timestamp);
-}
